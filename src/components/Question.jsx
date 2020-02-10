8e40e5a24781eef19a7b2df5c1ba87f0af1b1f3a
upoma bhuiyan <upomab8901@queensvoc.org>
2020-02-10 18:26:57 -0500

    d

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,22 +1,42 @@
 import React, { Component } from 'react';
 // import components
 
+import firebase from 'clients/firebase.js';
 
 class Question extends Component {
   constructor(props){
     super(props);
-    this.state={
 
-      question: "Which movie is Tadashi Hamada from?",
-      choices:[
-        "Big Heroe 6",
-        "UP!",
-        "Star Wars",
-        "Frozen 2"]
-    };
+    var database = buildFirebase();
+    var databaseRef = database.ref("/questions");
+    databaseRef.once("value").then(function(data) {
+      const questions = data.val();
+      // Do something with the questions
+
+      this.state={
+
+        question: questions[0].question_text,
+        choices:[
+          questions[0].choices[0],
+          questions[0].choices[1],
+          questions[0].choices[2],
+          questions[0].choices[3]]
+      };
+    });
 
   }
+//    class DataBase {
+// constructor (){
   
+// }
+
+
+
+
+//    }
+
+
+
   render() {
     return (
       <div>
