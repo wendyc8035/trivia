c8c275ac3df3df667ecd79c90380559bc38b7aea
upoma bhuiyan <upomab8901@queensvoc.org>
2020-03-02 18:27:14 -0500

    displayed question and answer, format the choice next time

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,51 +1,67 @@
 import React, { Component } from 'react';
 // import components
 
-import {buildFirebase} from '../clients/firebase.js';
+import { buildFirebase, getRandomQuestion } from '../clients/firebase.js';
 
 class Question extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
+
+    this.state = null;
 
     var database = buildFirebase();
     var databaseRef = database.ref("/questions");
-    databaseRef.once("value").then(function(data) {
+    databaseRef.once("value").then((data) => {
       const questions = data.val();
       // Do something with the questions
       console.log(questions);
+      this.setState(
 
-      this.state={
+        { question: getRandomQuestion(questions) }
 
-        question: questions[0].question_text,
-        choices:[
-          questions[0].choices[0],
-          questions[0].choices[1],
-          questions[0].choices[2],
-          questions[0].choices[3]]
-      };
+      )
+      // questions[0].question_text,
+      // choices:[
+      //   questions[0].choices[0],
+      //   questions[0].choices[1],
+      //   questions[0].choices[2],
+      //   questions[0].choices[3]]
     });
 
   }
-//    class DataBase {
-// constructor (){
-  
-// }
+  //    class DataBase {
+  // constructor (){
+
+  // }
 
 
 
 
-//    }
+  //    }
 
 
 
   render() {
-    return (
-      <div>
-        Add your QuestionText, Reset Button and AnswerButtons here. 
-       {this.state.question}
+    if (this.state
+      //  && this.state.question 
+    ) {
+      return <div>
+        {this.state.question.question_text}
+        <div>
+          <div id="one"> <button> {this.state.question.choices[0]}</button>
+            <button> {this.state.question.choices[1]}</button>
+            <button>  {this.state.question.choices[2]}</button>
+            <button>  {this.state.question.choices[3]}</button>
+          </div>
+        </div>
+
+
       </div>
-    );
+      // return <div>aaaaa</div>
+    }
+    else {
+      return <div>We got nothing!</div>;
+    }
   }
 }
-
 export default Question;
